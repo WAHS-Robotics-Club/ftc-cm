e6b2b1a495e804d06f2441d9edd553953aad9561
zjdt4 <party14534@gmail.com>
2018-09-26 17:01:12 -0400

    This is to upload LearnTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LearnTest.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class LearnTest {
+
+DcMotor fr, fl, br, bl;
+Servo sl, sr;
+
+        public LearnTest() {
+
+        }
+}
