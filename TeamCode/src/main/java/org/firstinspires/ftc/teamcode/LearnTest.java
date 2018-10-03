1be68f6b9e0f92241b0c47f887c22f16ab1a208a
zjdt4 <party14534@gmail.com>
2018-10-03 16:20:48 -0400

    This is an upload to LearnTest it added the if statements

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LearnTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LearnTest.java
@@ -1,14 +1,37 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-public class LearnTest {
+public class LearnTest extends OpMode {
+    DcMotor fr, fl, br, bl;
+    Servo sl, sr;
 
-DcMotor fr, fl, br, bl;
-Servo sl, sr;
+    @Override
+    public void init() {
+        fr = hardwareMap.dcMotor.get("fr");
+        fl = hardwareMap.dcMotor.get("fl");
+        br = hardwareMap.dcMotor.get("br");
+        bl = hardwareMap.dcMotor.get("bl");
 
-        public LearnTest() {
+        sl = hardwareMap.servo.get("sl");
+        sr = hardwareMap.servo.get("sr");
+
+        sl.setPosition(.18);
 
         }
+
+    @Override
+    public void loop() {
+
+        if (gamepad1.left_stick_y > .01){
+            
+        }
+        if (gamepad1.right_stick_y > .01) {
+
+        }
+
+
+    }
 }
