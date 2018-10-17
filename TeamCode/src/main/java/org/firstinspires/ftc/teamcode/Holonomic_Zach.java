efad71acb5c42fdeceea71495a7e86c984fe5705
zjdt4 <party14534@gmail.com>
2018-10-17 16:45:34 -0400

    Added Holonomic_Zach

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Holonomic_Zach.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+@TeleOp(name = "Holonomic_Zach")
+public class Holonomic_Zach extends OpMode {
+    DcMotor fr, fl, br, bl;
+
+    @Override
+    public void init() {
+        fr = hardwareMap.dcMotor.get("fr");
+        fl = hardwareMap.dcMotor.get("fl");
+        br = hardwareMap.dcMotor.get("br");
+        bl = hardwareMap.dcMotor.get("bl");
+
+    }
+
+    @Override
+    public void loop() {
+        
+
+        if (Math.abs(gamepad1.left_stick_y) > 0.1 || gamepad1.left_stick_x > .01) {
+        }
+    }
+}
