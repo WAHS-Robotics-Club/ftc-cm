5af3da676be561e50b1429b1a03d9cad7930a7d8
zjdt4 <party14534@gmail.com>
2018-11-13 15:58:41 -0500

    worked on teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Controller.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Controller.java
@@ -5,19 +5,27 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp (name = "Blue Red OpMode")
+@TeleOp (name = "Player Control")
 public class TeleOp_Controller extends OpMode {
 
-    DcMotor fr, fl, br, bl;
+    DcMotor fr, fl, br, bl; //lift;
     Servo S1, S2;
     Object x, y, z;
 
     @Override
     public void init() {
+
         fl = hardwareMap.dcMotor.get("fl");
         fr = hardwareMap.dcMotor.get("fr");
         bl = hardwareMap.dcMotor.get("bl");
         br = hardwareMap.dcMotor.get("br");
+        //       lift = hardwareMap.dcMotor.get("lift");
+
+        fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
     }
 
@@ -27,12 +35,12 @@ public class TeleOp_Controller extends OpMode {
         float y = -gamepad1.left_stick_y;
         float r = gamepad1.right_stick_x;
 
-        if (gamepad1.left_stick_x > .01 || gamepad1.left_stick_y > .01) {
+        if (Math.abs(gamepad1.left_stick_x) > .1 || Math.abs(gamepad1.left_stick_y) > .1 || Math.abs(gamepad1.right_stick_x) > .1) {
             fl.setPower(-x - y - r);
             fr.setPower(-x + y - r);
             bl.setPower(x - y - r);
             br.setPower(x + y - r);
-        } else {
+        }else {
             fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -40,5 +48,12 @@ public class TeleOp_Controller extends OpMode {
         }
 
 
+//        if (gamepad2.right_bumper = true){
+//            lift.setPower(.6);
+//        }else {
+//            if (gamepad2.left_bumper = true){
+//                lift.setPower(-.4);
+//            }
+//        }
     }
 }
