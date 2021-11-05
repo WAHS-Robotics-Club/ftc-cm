a1b7ae78388418ba37794151b8308c8062306b88
Charlie <calhusen@gmail.com>
2021-11-05 16:25:28 -0400

    documentation update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SingleDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SingleDriver.java
@@ -10,6 +10,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name ="Single Driver TeleOp")
 public class SingleDriver extends OpMode {
     //Local DcMotor variables:
+    //Creates the code objects for the motors
     DcMotor frontLeftMotor;
     DcMotor backLeftMotor;
     DcMotor frontRightMotor;
@@ -18,7 +19,8 @@ public class SingleDriver extends OpMode {
     //Initiation process:
     @Override
     public void init(){
-        //Hardwaremap ALL motors:
+        //Hardwaremaps ALL motors:
+        //This connects the motor objects to the hardware map on the phone
         frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
         backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
         frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
@@ -28,6 +30,7 @@ public class SingleDriver extends OpMode {
     //Loop process:
     @Override
     public void loop(){
+        //the format "motor name" (set up above) .setPower(); sets the motor speed, the range of inputs is from 0 (not moving) to 1 (full speed)
         frontLeftMotor.setPower(-gamepad1.left_stick_y);
         backLeftMotor.setPower(-gamepad1.left_stick_y);
         frontRightMotor.setPower(gamepad1.left_stick_y);
