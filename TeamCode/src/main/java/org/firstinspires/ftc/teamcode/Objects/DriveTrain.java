d3465d0f8ee04b3cd2c5c1fe9f4859a8b4292843
Charlie <calhusen@gmail.com>
2022-01-13 19:26:26 -0500

    oom.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Objects/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Objects/DriveTrain.java
@@ -28,6 +28,12 @@ public class DriveTrain{
         driveTrain.frMotor = hardwareMap.dcMotor.get("frontRightMotor");
         driveTrain.blMotor = hardwareMap.dcMotor.get("backLeftMotor");
         driveTrain.brMotor = hardwareMap.dcMotor.get("backRightMotor");
+
+        driveTrain.flMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        driveTrain.frMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        driveTrain.blMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        driveTrain.brMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         driveTrain.toggleSpeed = new Toggle();
 
         return driveTrain;
