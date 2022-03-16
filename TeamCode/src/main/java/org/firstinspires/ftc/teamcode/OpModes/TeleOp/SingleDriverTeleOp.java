c9b8a7907b68680edd505ad6fa0200ebeb575e62
Charlie <calhusen@gmail.com>
2022-03-16 17:26:45 -0400

    stripping away old code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/SingleDriverTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/SingleDriverTeleOp.java
@@ -11,16 +11,12 @@ import org.firstinspires.ftc.teamcode.Objects.Misc;
 @TeleOp(name ="Single Driver TeleOp - CM", group = "TeleOp")
 public class SingleDriverTeleOp extends OpMode {
     //Initializing the main objects:
-    Grabber grabber;
     DriveTrain driveTrain;
-    Misc misc;
 
     @Override
     public void init(){
         //Hardware mapping the servos:
-        grabber = Grabber.initGrabber(hardwareMap);
         driveTrain = DriveTrain.initDriveTrain(hardwareMap, DcMotor.ZeroPowerBehavior.FLOAT);
-        misc = Misc.initMiscellaneous(hardwareMap);
 
         driveTrain.resetEncoders();
         driveTrain.setRunMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -31,18 +27,6 @@ public class SingleDriverTeleOp extends OpMode {
         driveTrain.manualDrive(gamepad1);
         driveTrain.checkToggleSpeed(gamepad1);
         DriveTrain.logTelemetry(telemetry, driveTrain);
-
-        //Grabber System (Servos)
-        grabber.ManualToggleGrabber(gamepad1);
-
-        //Spool controls
-        grabber.ManualSpoolMotor(gamepad1);
-        telemetry.addData("Spool Motor Position", grabber.spoolMotor.getCurrentPosition());
-
-        //Misc controls
-        misc.toggleCarouselPowerManual(gamepad1);
-        misc.toggleCarouselDirectionManual(gamepad1);
-        misc.runCarouselServo();
 
     }
 
